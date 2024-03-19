621379f5df6611c67f94bae40a2d2aedb68b62b9
fakhrul <fakhrulhadi0607@gmail.com>
2024-03-19 11:53:55 +0800

    first delete

--- padam.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include('config.php');
-$id_pelajar = $_GET['id_pelajar'];
-$query = mysqli_query($connect,"DELETE FROM pelajar WHERE id_pelajar = '$id_pelajar'");
-header('Location:index.php');
-
-?>
